17282cf20f99ca1dac02eb0434f0ae53ae3da3c7
RaulGrosan <57448631+RaulGrosan@users.noreply.github.com>
2021-01-05 02:45:34 +0200

    final
    :D

---
+++ IP3FINAL/Assets/scripts/enemee.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemee : MonoBehaviour
+{
+    float timer = 0;
+    float timeToMove = 0.5f;
+    int numOfMovements = 0;
+    float speed = 0.25f;
+    public GameObject enemyProjectile;
+    public GameObject enemyProjectileClone;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (GameManager.playGame)
+        {
+            if (timer > timeToMove && numOfMovements < 9)
+            {
+                transform.Translate(new Vector3(speed, 0, 0));
+                timer = 0;
+                numOfMovements++;
+            }
+            if (numOfMovements == 9)
+            {
+                transform.Translate(new Vector3(0, -0.5f, 0));
+                numOfMovements = -1;
+                speed = -speed;
+                timer = 0;
+            }
+            fireEnemy();
+        }
+    }
+    void fireEnemy()
+    {
+        if(Random.Range(0f, 775f) < 1)
+        {
+            enemyProjectileClone = Instantiate(enemyProjectile, new Vector3(enemy.transform.position.x, enemy.transform.position.y - 0.6f, 0), enemy.transform.rotation);
+        }
+    }
+}
---
+++ IP3FINAL/Assets/scripts/enemy_projectile_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_projectile_script : MonoBehaviour
+{
+    public GameObject enemyprojectil;
+    Vector3 respawn = new Vector3(7, 4, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -3 * Time.deltaTime, 0));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            collision.gameObject.transform.position = respawn;
+            Destroy(enemyprojectil);
+            GameManager.lives--;
+            GameManager.playGame = false;
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            Destroy(enemyprojectil);
+        }
+    }
+}
